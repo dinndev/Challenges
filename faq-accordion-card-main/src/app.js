19d2be3c9e87074e232483015bcdeef306b83d5d
din <dinndev@gmail.com>
2020-10-20 16:22:06 +0800

    Change the functionality with css

--- faq-accordion-card-main/src/app.js
+++ faq-accordion-card-main/src/app.js
@@ -1,11 +1,21 @@
 const accordion = document.querySelectorAll('.accordion');
 const panels = document.querySelectorAll('.panel');
-accordion.forEach((button , index) => {
+
+accordion.forEach(button  => {
  button.addEventListener('click' , function () {
-     this.classList.toggle('active');
-    const panel = button.nextElementSibling
-    panel.style.display === 'block' ?
-    panel.style.display = "none" :
-     panel.style.display = 'block';
+    const panel = this.nextElementSibling;
+    console.log(panel)
+    if(panel.style.maxHeight){
+        panel.style.maxHeight = null ;
+    } else {
+        let active = document.querySelectorAll('a.accordion.active');
+         for(let i = 0; i < active.length; i++){
+            active[i].classList.remove('active');
+            active[i].nextElementSibling.style.maxHeight = null;
+         }
+    
+    }
+    this.classList.toggle('active');
+    panel.style.maxHeight = panel.scrollHeight + 'px';
  })
 })
